639a3cc662c7d34be5debc7928184c0ea7e60ff6
Mehedi86 <abdullahalmehedi@gmail.com>
2025-04-16 08:51:12 +0600

    added: third commit

--- app/page.jsx
+++ app/page.jsx
@@ -2,16 +2,25 @@
 import DuaCard from "@/components/DuaContent/DuaCard";
 import DuaSection from "@/components/DuaContent/DuaSection";
 import useFetch from "@/hooks/useFetch";
+import { useEffect, useState } from "react";
 
 
 export default function Home() {
-  const {duas} = useFetch();
-  console.log(duas)
-   return (
+  const { duas } = useFetch();
+  const [requireDuas, setRequiredDuas] = useState([]);
+
+  useEffect(() => {
+    if (duas && duas.length > 0) {
+      setRequiredDuas(duas);
+    }
+  }, [duas]);
+
+
+  return (
     <div>
       <DuaSection />
       <div className="">
-        {duas.map((dua, index)=> <DuaCard key={dua._id} dua={dua} index={index}/>)}
+        {requireDuas.map((dua, index) => <DuaCard key={dua._id} dua={dua} index={index} />)}
       </div>
     </div>
   );
