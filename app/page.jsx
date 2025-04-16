996c9646f3602b726e9044834dcde24d9a07b226
Mehedi86 <abdullahalmehedi@gmail.com>
2025-04-16 08:57:35 +0600

    added: scrollable functioinality added

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,8 @@
-'use client'
+'use client';
 import DuaCard from "@/components/DuaContent/DuaCard";
 import DuaSection from "@/components/DuaContent/DuaSection";
 import useFetch from "@/hooks/useFetch";
 import { useEffect, useState } from "react";
-
 
 export default function Home() {
   const { duas } = useFetch();
@@ -15,13 +14,15 @@ export default function Home() {
     }
   }, [duas]);
 
-
   return (
-    <div>
+    <div className="h-screen overflow-y-auto px-4 no-scrollbar">
       <DuaSection />
-      <div className="">
-        {requireDuas.map((dua, index) => <DuaCard key={dua._id} dua={dua} index={index} />)}
+      <div>
+        {requireDuas.map((dua, index) => (
+          <DuaCard key={dua._id} dua={dua} index={index} />
+        ))}
       </div>
     </div>
   );
 }
+
